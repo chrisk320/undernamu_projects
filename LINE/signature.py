7dc81dbdce90eb8eceac4703922240ea4a595553
Christian Kim <chris.kim320@gmail.com>
2024-06-30 23:01:49 +0900

    update .gitignore and signature.py to SK Medical tokens

--- LINE/signature.py
+++ LINE/signature.py
@@ -4,27 +4,27 @@ import time
 
 privateKey = {
   "alg": "RS256",
-  "d": "uOPF3u59m8catXs59TZh8adF8spZ266uOJbo3gEYs-DuwXqLCToUYM7kZfh8mUy8IZWRj-bC2Ibx2-wJ-eT2HGpaFLvAj37OB1alCqCij_S5BVVSBe23NszRJd6SaHYWCXkEwCFgvA9KZB-YT_nnCpXdWHrnwzTwG9tUj-pyaOfvrUoeOFmbXn1yY7w-AF2cFGVcWJYt6N67BupuVw3g-lusoWjlIdJq8bheuPqr6VTv6YGCJuKN30i7QUVnmhN-F1T99a0QFh0J0GrX3vyc4EhtF8erDRpT-FlvOkASZ79SxEH9BqiqKkc7RICCWpj5oHcvumk2lCXbnmS7C3dDAQ",
-  "dp": "XRiy6dXxQyASHJJNLs1e2GB9rpiiqP-C4bTy5HGWumf5I-OnVHp_MVD8gH35O6-QccAQ0vcxRRprhOdn4Dbjp_Z_xEFzv8kLyhqJ4TlT_Gy_rbzNj7hj4Zh8W7wUeSKQutoRQe2Y2CSzQThzxXb7ZSnO6mObdAmyU0tM2jTdT2M",
-  "dq": "KaG--zSKCSbl6SqmdwaOehDFcj0BVH_7mDbRczgbaSFWcT2_1p6P8_e9_ut9nq7q72NSo7tyAPNACo4Kh6i0guwomMKgv_84shA-Pq7Ow3QEIVmK8-2sf9MMa5yhS-9SL4mKOEqV61MouXDs19qsCaerWtgdMHTI0hZ0apQJpQE",
+  "d": "mcf2gbjExYkJbL5xmVzOPBxDofAx3n2rg7Y7zmlE5zKv1zdvOLy0LLUIl4EuX5kIvvuSSpgECv-kBpw9GIrDyy9ja8iPkFKKpeBJDmmDJyAY1JT9jV49CllMNXGPj5pK02Z77uULZtp9KAwU1evRF7OSaYnQVYRqT9THNR86tGnbQiHcQd_gr-VMkjKBl6K7o8qVyOU4zWWQhk3XCXAwMDQKDy11KRXN-_seEwl6ASITBvnnoYpbiKDd2CcFNs6fl30NcR4WP5NZ5gJ75jBA4feIaK24mz1--j8JQrMrj0ZfUB91T-cyjqF-7v-_uDCXWWaqN5aeNmb-BEqsXr2ooQ",
+  "dp": "jnBmpQuJVP4YW1KCzisliJCXriNCE37AMYS4XEEhj34povQczpW-bz8Ain78T58YaTi4-pzwJzNtvM7uhhRgUgRJB5iYMpDoiGaLMP0F3S8h0O9xxCze0C4nvKvo8uryGM1vAp85FWWO_Q9g-EJiw6SvZ4n8NZ4Ntw5pN7A88Ls",
+  "dq": "eDMfMmAY-_nYY3sk05V5_WQGCkZ2bJO7hfg5MWds21uP5Xqh9ykUqb7tpbpygGzBWHIXprjEuty8PVqdoU65BJsQiBHIZklSnEcta6YneQjCCtB4bq1AXvsD7aAJppuU-S4jbxt6BXTUZmuC98UgJBw1iXneB7TxYyORlBM6LME",
   "e": "AQAB",
   "kty": "RSA",
-  "n": "xRmby-7QBp2ba90rK_IfGz4qQhYhi-4ztR3ROUC8G4HFNCt52LQMWsaPebwLFYe0EnIrWuJVXuEmPBjCeWoD7am4f2GEesX-1apx3Jpx_f1p-VJ1zQm2HyPEB9gXg-B2dczU7TMJDgERYgdFvmPxrWUq_xKLRD_TTAGNAf2RyehYfIar-WlGy7dMgU_6BqLg1UljuNmz-e4k0FxnhIWpNw5XJxW9HGbzLyw99pcctQkV8QECxouBtE2e1MY2Zu0caWZIbRCiejypEUHZ-Vh4ObOK6d2QU6w6dOjiQeBoZ6ACd4uBC8r9pVd6Q_MDtsSg0u2rGeWKhKl_5O60t717iw",
-  "p": "_51UBCup0tx1jh_JcSVqndi0FoFAkpKRxMYJY4Y9Ebx9nLbruFURnph3tYz62-WTEUMoHNRclUqkteNUQ445wO_7_zKtObMNg-8uxSRs0LhuignhkUW948D07FhrvK2jwcfoiH-H8HGNU0Gz2aIe6V86327i4JOlqVYXwwNwBws",
-  "q": "xWWxVu37vAHoqhvsUxFmlPh4qnC_wmLBvEHRcDryNMMhpSzz0eO_S_fjFQfTcPPE2VUfulMnwPog7VUISrzFbdfWTqnnhsv3391VgtDyJqZQCXCT04-MFcXGHW5dukwyzQ_Gi0tZH1IfuCsV6A_HLtprsRcBAr2bTEHCdGVHLYE",
-  "qi": "agZo7bBTN2lI9fESai79cGg5KSETfUsd4dP0JlJmTvfQVNt0ufnA3wnXTeEXp683QQBA7drWlKaf3Ipy8sOPC5OYMFXZy4iCuMPki5hnSBsMFd5uWlJ3sGDgi56YmQXahr8QxFCr3qzotd2LHRTc2BiM7sRGy0U8hhCcBoBNwUI",
+  "n": "wZR2yUYYkUK3HMoBvV18uiyn8Ld8fXaEccpuNypglZivxFKAaBDIHTGnkDaMx-1bIQoo_WuGme8NAcvJ4E22OSmTxSbd2t3OY2i5d6Qn6RUg7uwFvDEfwLLbqUVNScBLwGsSaVvoR4EUULSy2vZpZCPx2BqFtn4kmCuox5aJaX8eMYu7OSHfQTrJB2PpeMtg4twDgS3UOVmdQAi14DO-R7QI8cuSFlQtF6-sNSYDwyXbggKsH1TzS0Vgs2FUGyilkuYniGTA321xFyvFKODvAwUC2sm8kLz6GBGYHwjvNzIV_gxtvQklMkXXNxqhW7sRrkabarxQ9YxhbldvdWU4Fw",
+  "p": "9d9HO50IgZBWJ3237NZBAtpe416-OuVzr6_oz8sDvxRWO69GWeyg3Ee2wec_CqSJGDvxpaW0-0-eCy8Uar4zYcseCbT6tv2kBmLHG9QcXHZsCC4_A8KNwIfg10BiKnAVfng4lciq3TgALfJ84sQcjHy03h0j5vWa4WjDXRRLAyc",
+  "q": "yY3Dnm05OKL-EeQ646P4Q3pnD0BuKiTJEd3h1ApdIglZN75xqtFPe6lGKvf1Rt23QCJFLFv8Cpgp4Zm8HYeZod0wC4sL3nmtt2Knb7bD6wqhko2NtclXMe7iXzyYhwQ93Pb9wUUmLfJ-M0yKCwEtrLZqQIAdWrKWEvLnmwEdeZE",
+  "qi": "nS-r3j17ZDiBIVlUkl-aNj_m8cCOZFTl0gqP9im92tvd7rXEhsumryOtWjT8lnE6h4yi-BOnzh3RVcFFTnECMlbHnvOdN9bxVemD71VACLUXJWEnBfJRLmk8uCckkB9_f5r2Al_lVEh2ZyZRuXS9a2hiR0opZFHXRLHLuE52I_g",
   "use": "sig"
 }
 
 headers = {
     "alg": "RS256",
     "typ": "JWT",
-    "kid": "875d3d0a-2f72-4f63-bd29-0e97e9b12d64"
+    "kid": "66493771-853b-4ffa-b9f3-9f3697ff6874"
 }
 
 payload = {
-  "iss": "2005708757",
-  "sub": "2005708757",
+  "iss": "2005723717",
+  "sub": "2005723717",
   "aud": "https://api.line.me/",
   "exp":int(time.time())+(60 * 30),
   "token_exp": 60 * 60 * 24 * 30
